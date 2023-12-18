a9f4ce291741a4417018d8c3e405468357d6d129
birchkwok <birchkwok@gmail.com>
2023-12-18 18:20:13 +0800

    Add logic for handling array shapes.

--- min_vec/engine.py
+++ min_vec/engine.py
@@ -1,9 +1,20 @@
 import numpy as np
 
 
+def squeeze(func):
+    def wrapper(vec1, vec2):
+        vec1 = vec1.squeeze()
+        vec2 = vec2.squeeze()
+
+        return func(vec1, vec2)
+    return wrapper
+
+
+@squeeze
 def cosine_distance(vec1, vec2):
     return np.dot(vec1, vec2)
 
 
+@squeeze
 def euclidean_distance(vec1, vec2):
     return np.linalg.norm(vec1 - vec2)
