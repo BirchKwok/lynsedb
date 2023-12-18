748017f08e91b7596628d795b28045aa98a3ccaa
birchkwok <birchkwok@gmail.com>
2023-12-18 18:22:27 +0800

    Add fault tolerance logic, add unified folder management for vector files, and add commit processing logic. All storage operations should be marked as completed with the commit method.

--- min_vec/model.py
+++ min_vec/model.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import shutil
 
 import numpy as np
 from spinesUtils.asserts import ParameterValuesAssert, ParameterTypeAssert
@@ -36,15 +37,32 @@ class MinVectorDB:
         self.dim = dim
         self.dtypes = dtypes
         self.distance = distance
+        self._EMPTY_DATABASE = np.zeros((1, self.dim), dtype=self.dtypes)
 
         self.chunk_size = chunk_size
-        self.database_path_parent = Path(database_path).parent
+        self.database_path_parent = Path('.mvdb'.join(Path(database_path).name.split('.mvdb')[:-1]))
         self.database_name_prefix = '.mvdb'.join(Path(database_path).name.split('.mvdb')[:-1])
+
+        if self.database_path_parent.exists():
+            raise FileExistsError("The database needs to create a folder with the same name."
+                                  f" Please ensure there is no folder named '{self.database_name_prefix}' "
+                                  f"in your current file path, or specify a different name for the database.")
+        else:
+            self.database_path_parent.mkdir(parents=True)
+
         self.all_indices = set()
         self.last_id = None
 
+        # parameters for reorganize the temporary files
+        self.temp_file_target = '.temp'
+
+        # ============== Loading or create one empty database ==============
+        # first of all, initialize a database
+        self.reset_database()
+
         # If they exist, iterate through all .mvdb files.
         self.database_chunk_path = []
+
         for i in os.listdir(self.database_path_parent):
             # If it meets the naming convention, add it to the chunk list.
             if i.startswith(self.database_name_prefix) and Path(i).name.split('.')[0].split('_')[-1].isdigit():
@@ -53,21 +71,17 @@ class MinVectorDB:
         # If database_chunk_path is not empty, define the loading conditions for the database and index.
         if len(self.database_chunk_path) > 0:
             self.chunk_id = max([int(Path(i).name.split('.')[0].split('_')[-1]) for i in self.database_chunk_path])
-            max_chunk_path = [i for i in self.database_chunk_path
-                              if int(Path(i).name.split('.')[0].split('_')[-1]) == self.chunk_id][0]
 
-            with open(max_chunk_path, 'rb') as f:
-                self.database = np.load(f, allow_pickle=True)
-                self.index = list(np.load(f, allow_pickle=True))
-                self.field = list(np.load(f, allow_pickle=True))
+            for chunk_id, (chunk_data, index, chunk_field) in enumerate(self._data_loader()):
+                self.database = chunk_data
+                self.indices = index
+                self.fields = chunk_field
+                for idx in index:
+                    self.all_indices.add(idx)
 
             if self.database.shape[0] == self.chunk_size:
                 self.reset_database()
                 self.chunk_id += 1
-
-            for _, index, _ in self._data_loader():
-                for idx in index:
-                    self.all_indices.add(idx)
         else:
             # If database_chunk_path is empty, define a new database and index.
             self.chunk_id = 0
@@ -76,20 +90,23 @@ class MinVectorDB:
     def reset_database(self):
         """Reset the database to its initial state with zeros."""
         self.database = np.zeros((1, self.dim), dtype=self.dtypes)
-        self.index = []
-        self.field = []
+        self.indices = []
+        self.fields = []
 
     def _data_loader(self):
         """Generator that yields database chunks and corresponding indices."""
         # By default, save the checkpoint first.
         self.save_checkpoint()
 
-        for i in self.database_chunk_path:
-            with open(i, 'rb') as f:
-                database = np.load(f, allow_pickle=True)
-                index = np.load(f, allow_pickle=True)
-                field = np.load(f, allow_pickle=True)
-            yield database, index, field
+        try:
+            for i in self.database_chunk_path:
+                with open(i, 'rb') as f:
+                    database = np.load(f, allow_pickle=True)
+                    index = np.load(f, allow_pickle=True)
+                    field = np.load(f, allow_pickle=True)
+                yield database, index, field
+        except FileNotFoundError:
+            raise FileNotFoundError("Did you forget to run `commit()` function ? Try to run `commit()` first.")
 
     def _add_path_to_chunk_list(self, path):
         """Add a new chunk's path to the list of database chunks.
@@ -105,22 +122,23 @@ class MinVectorDB:
 
     def _is_database_reset(self):
         """Check if the database is in its reset state (filled with zeros)."""
-        return (np.zeros((1, self.dim), dtype=self.dtypes) == self.database).all()
+        return (self._EMPTY_DATABASE == self.database).all()
 
     def _length_checker(self):
         """Check if the length of the database and index are equal."""
-        if np.mean([self.database.shape[0], len(self.index), len(self.field)]) != self.database.shape[
+        if np.mean([self.database.shape[0], len(self.indices), len(self.fields)]) != self.database.shape[
             0] and not self._is_database_reset():
             raise ValueError('The database, index length and field length not the same.')
 
     def _save(self):
         self._length_checker()
-        database_name = self.database_path_parent / f'{self.database_name_prefix}_{self.chunk_id}.mvdb'
+        database_name = (self.database_path_parent /
+                         f'{self.database_name_prefix}_{self.chunk_id}.mvdb{self.temp_file_target}')
 
         with open(database_name, 'wb') as f:
-            np.save(f, self.database)
-            np.save(f, self.index)
-            np.save(f, self.field)
+            np.save(f, self.database, allow_pickle=True)
+            np.save(f, self.indices, allow_pickle=True)
+            np.save(f, self.fields, allow_pickle=True)
 
         return database_name
 
@@ -132,7 +150,9 @@ class MinVectorDB:
             self.chunk_id += 1
 
             # Storage path for .mvdb file chunks.
-            self._add_path_to_chunk_list(database_name)
+            self._add_path_to_chunk_list(
+                Path(self.temp_file_target.join(str(database_name).split(self.temp_file_target)[:-1]))
+            )
             # Reset the database.
             self.reset_database()
 
@@ -144,10 +164,15 @@ class MinVectorDB:
         if not self._is_database_reset():
             database_name = self._save()
 
-            self._add_path_to_chunk_list(database_name)
+            self._add_path_to_chunk_list(
+                Path(self.temp_file_target.join(str(database_name).split(self.temp_file_target)[:-1]))
+            )
 
     def commit(self):
-        """Save the database, ensuring that all data is written to disk."""
+        """Save the database, ensuring that all data is written to disk.
+        Note that, After saving vectors, a commit is required to query.
+
+        """
         # If this method is called, the part that meets the chunk size will be saved first, 
         #  and the part that does not meet the chunk size will be directly saved as the last chunk.
         self.save_checkpoint()
@@ -155,6 +180,9 @@ class MinVectorDB:
         # Initialize the database.
         if not self._is_database_reset():
             self.reset_database()
+
+        for fp in self.database_chunk_path:
+            os.rename(str(fp) + self.temp_file_target, fp)
 
     @ParameterTypeAssert({'vectors': (tuple, list)})
     @ParameterValuesAssert({'vectors': lambda s: all(1 <= len(i) <= 3 and isinstance(i, tuple) for i in s)})
@@ -211,8 +239,8 @@ class MinVectorDB:
         if id is None:
             if self.last_id is None:
                 id = -10000
-                if self.database_chunk_path == []:
-                    id = max(id, len(self.index), 0 if len(self.index) == 0 else max(self.index))
+                if not self.database_chunk_path:
+                    id = max(id, len(self.indices), 0 if len(self.indices) == 0 else max(self.indices))
                 else:
                     for _, index in self._data_loader():
                         id = max(id, max(index))
@@ -228,8 +256,8 @@ class MinVectorDB:
         else:
             self.database = np.vstack((self.database, vector))
 
-        self.index.append(id)
-        self.field.append(field)
+        self.indices.append(id)
+        self.fields.append(field)
         self.all_indices.add(id)
         self.last_id = id
 
@@ -273,6 +301,9 @@ class MinVectorDB:
                 database = database[np.isin(vector_field, field)]
                 index = index[np.isin(vector_field, field)]
 
+            if len(index) == 0:
+                continue
+
             # Distance calculation core code
             if self.distance == 'cosine':
                 batch_scores = cosine_distance(database, vector).squeeze()
@@ -293,16 +324,7 @@ class MinVectorDB:
     @property
     def shape(self):
         """Return the shape of the entire database."""
-        self.save_checkpoint()
-
-        if len(self.database_chunk_path) == 0:
-            return None
-
-        length = 0
-        for database, *_ in self._data_loader():
-            length += database.shape[0]
-
-        return length, self.dim
+        return len(self.all_indices), self.dim
 
     @ParameterTypeAssert({'n': int})
     def head(self, n=10):
@@ -335,7 +357,6 @@ class MinVectorDB:
         if len(self.database_chunk_path) == 0:
             return None
 
-        for fp in self.database_chunk_path:
-            os.remove(fp)
+        shutil.rmtree(self.database_path_parent)
 
         self.reset_database()
