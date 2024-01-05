a8ade40a1262a9135e6bd612eaaf9ca6c095353c
birchkwok <birchkwok@gmail.com>
2024-01-05 14:40:03 +0800

    Switch the computation backend to PyTorch to enable GPU acceleration.

--- min_vec/engine.py
+++ min_vec/engine.py
@@ -1,20 +1,40 @@
 import numpy as np
+import torch
 
 
-def squeeze(func):
-    def wrapper(vec1, vec2):
-        vec1 = vec1.squeeze()
-        vec2 = vec2.squeeze()
-
-        return func(vec1, vec2)
-    return wrapper
+def get_device(device='auto'):
+    if device == 'auto':
+        return torch.device("cuda" if torch.cuda.is_available()
+                            else ("mps" if torch.backends.mps.is_available() else "cpu"))
+    return torch.device(device)
 
 
-@squeeze
-def cosine_distance(vec1, vec2):
-    return np.dot(vec1, vec2)
+def to_normalize(vec: np.ndarray, device='auto'):
+    if vec.ndim == 1:
+        norm = np.linalg.norm(vec)
+        if norm == 0:
+            return vec
+        return vec / norm
+    elif vec.ndim == 2:
+        # 每一行求归一化, 使用pytorch实现
+        device = get_device(device)
+        vec = torch.from_numpy(vec).to(device)
+        norm = torch.norm(vec, dim=1, keepdim=True)
+        return (vec / norm).detach().cpu().numpy()
+    else:
+        raise ValueError("vec must be 1d or 2d array")
 
 
-@squeeze
-def euclidean_distance(vec1, vec2):
-    return np.linalg.norm(vec1 - vec2)
+def cosine_distance(vec1, vec2, device='auto'):
+    device = get_device(device)
+    vec1 = torch.from_numpy(vec1).to(device).squeeze()
+    vec2 = torch.from_numpy(vec2).to(device).squeeze()
+    return torch.matmul(vec1, vec2).detach().cpu().numpy()
+
+
+def euclidean_distance(vec1, vec2, device='auto'):
+    device = get_device(device)
+    # 使用pytorch实现欧氏距离
+    vec1 = torch.from_numpy(vec1).to(device).squeeze()
+    vec2 = torch.from_numpy(vec2).to(device).squeeze()
+    return torch.norm(vec1 - vec2).detach().cpu().numpy()
