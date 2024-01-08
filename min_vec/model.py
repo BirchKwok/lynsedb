21294def493a73606d6e730e19093d13c8354720
birchkwok <birchkwok@gmail.com>
2024-01-08 18:10:44 +0800

    Performance optimization: Use multi-processing for accelerated querying.

--- min_vec/model.py
+++ min_vec/model.py
@@ -3,11 +3,14 @@ import os
 import shutil
 
 import numpy as np
+import torch
 from spinesUtils.asserts import ParameterValuesAssert, ParameterTypeAssert
+from concurrent.futures import ThreadPoolExecutor
 
 from min_vec.session import DatabaseSession
-from min_vec.engine import cosine_distance, euclidean_distance, to_normalize
+from min_vec.engine import cosine_distance, euclidean_distance, to_normalize, get_device
 from min_vec.filter import BloomTrie
+from min_vec.utils import io_assert
 
 
 class MinVectorDB:
@@ -15,19 +18,13 @@ class MinVectorDB:
     A class for managing a vector database stored in .mvdb files and computing vectors similarity.
 
     Attributes:
-        dim (int): Dimension of the vectors in the database.
-        dtypes (data-type): Data type of the vectors (default: np.float32).
-        distance (str): Method for calculating vector distance ('cosine' or 'L2').
-        database_path_parent (Path): Path to the parent directory of the database.
-        database_name_prefix (str): Prefix for naming the database files.
-        chunk_size (int): Size of each data chunk in the database.
-        last_id (int): Last ID used in the database.
-        temp_file_target (str): Suffix for temporary files during operations.
-        _database_chunk_path (list): Paths to the database chunks.
-        chunk_id (int): Identifier for the current data chunk.
-        database (np.array): The numpy array representing the database.
-        indices (list): List of indices in the current database chunk.
-        fields (list): List of fields associated with each vector in the chunk.
+        dim (int): Dimension of the vectors.
+        database_path (str): Path to the database file.
+        chunk_size (int): The size of each data chunk.
+        dtypes (data-type): Data type of the vectors.
+        distance (str): Method for calculating vector distance. Options are 'cosine' or 'L2' for Euclidean distance.
+        bloom_filter_size (int): The size of the bloom filter.
+        device (str): The device to use for vector operations. Options are 'auto', 'cpu', 'mps', or 'cuda'.
     """
 
     @ParameterTypeAssert({
@@ -35,7 +32,7 @@ class MinVectorDB:
     }, func_name='MinVectorDB')
     @ParameterValuesAssert({
         'database_path': lambda s: s.endswith('.mvdb'),
-        'distance': ('cosine', 'L2')
+        'distance': ('cosine', 'L2'),
     }, func_name='MinVectorDB')
     def __init__(self, dim, database_path, chunk_size=100_000, dtypes=np.float32, distance='cosine',
                  bloom_filter_size=100_000_000, device='auto') -> None:
@@ -45,11 +42,14 @@ class MinVectorDB:
         Parameters:
             dim (int): Dimension of the vectors.
             database_path (str): Path to the database file.
-            chunk_size (int): The size of each data chunk.
-            dtypes (data-type): Data type of the vectors.
-            distance (str): Method for calculating vector distance. Options are 'cosine' or 'L2' for Euclidean distance.
-            bloom_filter_size (int): The size of the bloom filter.
-            device (str): The device to use for vector operations. Options are 'auto', 'cpu', 'mps', or 'cuda'.
+            chunk_size (int): The size of each data chunk. Default is 100_000.
+            dtypes (str): Data type of the vectors.
+                Default is 'float32'. Options are 'float32', 'float64', 'float16', 'int32', 'int64', 'int16', 'int8'.
+            distance (str): Method for calculating vector distance.
+                Options are 'cosine' or 'L2' for Euclidean distance. Default is 'cosine'.
+            bloom_filter_size (int): The size of the bloom filter. Default is 100_000_000.
+            device (str): The device to use for vector operations.
+                Options are 'auto', 'cpu', 'mps', or 'cuda'. Default is 'auto'.
 
         Raises:
             ValueError: If `chunk_size` is less than or equal to 1.
@@ -59,9 +59,10 @@ class MinVectorDB:
 
         self.dim = dim
         self.dtypes = dtypes
+
         self.distance = distance
         self._EMPTY_DATABASE = np.zeros((1, self.dim), dtype=self.dtypes)
-        self.device = device
+        self.device = get_device(device)
 
         # list for add item
         self._tmp_vectors = []
@@ -153,23 +154,27 @@ class MinVectorDB:
         self.indices = []
         self.fields = []
 
-    def _data_loader(self):
+    @io_assert
+    def _data_loader(self, paths=None):
         """
         Generator that yields database chunks and corresponding indices.
+
+        Parameters:
+            paths (list, optional): List of paths to the database chunks.
 
         Yields:
             Tuple containing database chunk, indices in the chunk, and field information.
         """
         # By default, save the checkpoint first.
-        try:
-            for i in self._database_chunk_path:
-                with open(i, 'rb') as f:
-                    database = np.load(f)
-                    index = np.load(f, allow_pickle=True)
-                    field = np.load(f, allow_pickle=True)
-                yield database, index, field
-        except FileNotFoundError:
-            raise FileNotFoundError("Did you forget to run `commit()` function ? Try to run `commit()` first.")
+        paths = paths if paths is not None else self._database_chunk_path
+        for i in paths:
+            with open(i, 'rb') as f:
+                database = np.load(f)
+                index = np.load(f, allow_pickle=True)
+                field = np.load(f, allow_pickle=True)
+
+            yield database, index, field
+
 
     def _add_path_to_chunk_list(self, path):
         """
@@ -204,6 +209,7 @@ class MinVectorDB:
                 and not self._is_database_reset()):
             raise ValueError('The database, index length and field length not the same.')
 
+    @io_assert
     def _save(self):
         """
         Save the current state of the database to a .mvdb file.
@@ -299,11 +305,35 @@ class MinVectorDB:
             self._COMMIT_FLAG = True
             self._NEVER_COMMIT_FLAG = False
 
+    def _generate_new_id(self):
+        """
+        Generate a new ID for the vector.
+        """
+        while True:
+            self.last_id += 1
+            if self.last_id not in self._id_filter:
+                break
+
+        return self.last_id
+
+    def _process_vector_item(self, vector, id, field, normalize):
+        if id in self._id_filter:
+            raise ValueError(f'id {id} already exists')
+
+        if len(vector) != self.dim:
+            raise ValueError(f'vector dim error, expect {self.dim}, got {len(vector)}')
+
+        vector = vector.astype(self.dtypes)
+        if normalize:
+            vector = to_normalize(vector)
+
+        return vector, id, field
+
     @ParameterTypeAssert({'vectors': (tuple, list), 'normalize': bool})
     @ParameterValuesAssert({'vectors': lambda s: all(1 <= len(i) <= 3 and isinstance(i, tuple) for i in s)})
     def bulk_add_items(self, vectors, normalize: bool = False, save_immediately=False):
         """
-        Bulk add vectors to the database.
+        Bulk add vectors to the database in batches.
 
         Parameters:
             vectors (list or tuple): A list or tuple of vectors to be saved. Each vector can be a tuple of (vector, id, field).
@@ -317,86 +347,58 @@ class MinVectorDB:
             self._initialize(insert=True)
             self._NEVER_COMMIT_FLAG = True
 
-        new_vectors = []
+        batch_size = 10000
         new_ids = []
-        new_fields = []
 
-        for sample in vectors:
-            if len(sample) == 1:
-                vector = sample[0]
-                id = None
-                field = None
-            elif len(sample) == 2:
-                vector, id = sample
-                field = None
-            else:
-                vector, id, field = sample
+        for i in range(0, len(vectors), batch_size):
+            batch = vectors[i:i + batch_size]
+            temp_vectors, temp_indices, temp_fields = [], [], []
 
-            vector = vector.astype(self.dtypes)
-
-            if id is not None and id in self._id_filter:
-                raise ValueError(f'id {id} already exists')
-
-            if len(vector) != self.dim:
-                raise ValueError(f'vector dim error, expect {self.dim}, got {len(vector)}')
-
-            new_vectors.append(vector)
-            new_ids.append(id if id is not None else self._generate_new_id())
-            new_fields.append(field)
-
-        # Convert lists to numpy arrays
-        new_vectors = np.array(new_vectors, dtype=self.dtypes)
-
-        if normalize:
-            new_vectors = to_normalize(new_vectors)
-
-        if save_immediately:
-            # Update database, indices, and fields
-            if self._is_database_reset():
-                self.database = new_vectors
-            else:
-                self.database = np.vstack((self.database, new_vectors))
-
-            self.indices.extend(new_ids)
-            self.fields.extend(new_fields)
-            for item in new_ids:
-                self._id_filter.add(item)
-
-            self.save_checkpoint()
-        else:
-            if self.database.shape[0] >= self.chunk_size:
-                # Update database, indices, and fields
-                if self._is_database_reset():
-                    self.database = new_vectors
+            for sample in batch:
+                if len(sample) == 1:
+                    vector = sample[0]
+                    id = None
+                    field = None
+                elif len(sample) == 2:
+                    vector, id = sample
+                    field = None
                 else:
-                    self.database = np.vstack((self.database, new_vectors))
+                    vector, id, field = sample
 
-                self.indices.extend(new_ids)
-                self.fields.extend(new_fields)
-                for item in new_ids:
+                vector, id, field = self._process_vector_item(vector, id, field, normalize)
+
+                temp_vectors.append(vector)
+                temp_indices.append(id if id is not None else self._generate_new_id())
+                temp_fields.append(field)
+
+            # Convert lists to numpy arrays
+            temp_vectors = np.array(temp_vectors, dtype=self.dtypes)
+
+            if save_immediately:
+                if self._is_database_reset():
+                    self.database = temp_vectors
+                else:
+                    self.database = np.vstack((self.database, temp_vectors))
+
+                self.indices.extend(temp_indices)
+                self.fields.extend(temp_fields)
+                for item in temp_indices:
                     self._id_filter.add(item)
 
                 self.save_checkpoint()
             else:
-                self._tmp_vectors.extend(new_vectors)
-                self._tmp_indices.extend(new_ids)
-                self._tmp_fields.extend(new_fields)
+                self._tmp_vectors.extend(temp_vectors)
+                self._tmp_indices.extend(temp_indices)
+                self._tmp_fields.extend(temp_fields)
+
+            new_ids.extend(temp_indices)
 
         if self._COMMIT_FLAG:
             self._COMMIT_FLAG = False
 
+        del temp_vectors, temp_indices, temp_fields
+
         return new_ids
-
-    def _generate_new_id(self):
-        """
-        Generate a new ID for the vector.
-        """
-        while True:
-            self.last_id += 1
-            if self.last_id not in self._id_filter:
-                break
-
-        return self.last_id
 
     @ParameterValuesAssert({'vector': lambda s: s.ndim == 1})
     @ParameterTypeAssert({
@@ -431,8 +433,7 @@ class MinVectorDB:
 
         id = self._generate_new_id() if id is None else id
 
-        vector = vector.astype(self.dtypes)
-        vector = to_normalize(vector) if normalize else vector
+        vector, id, field = self._process_vector_item(vector, id, field, normalize)
 
         # Add the id to then bloom filter.
         self._id_filter.add(id)
@@ -471,6 +472,46 @@ class MinVectorDB:
 
         return id
 
+    def _query_chunk(self, database_chunk, index_chunk, vector, field, subset_indices, vector_field):
+        """
+        Query a single database chunk for the vectors most similar to the given vector.
+
+        Parameters:
+            database_chunk (np.ndarray): The database chunk to be queried.
+            index_chunk (np.ndarray): The indices of the vectors in the database chunk.
+            vector (np.ndarray): The query vector.
+            field (str or list, optional): The target field for filtering the vectors.
+            subset_indices (list, optional): The subset of indices to query.
+
+        Returns:
+            Tuple: The indices and similarity scores of the nearest vectors in the chunk.
+        """
+        if field is not None:
+            if isinstance(field, str):
+                field = [field]
+
+            database_chunk = database_chunk[np.isin(vector_field, field)]
+            index_chunk = index_chunk[np.isin(vector_field, field)]
+
+        if subset_indices is not None:
+            subset_indices = list(set(subset_indices))
+            database_chunk = database_chunk[np.isin(index_chunk, subset_indices)]
+            index_chunk = index_chunk[np.isin(index_chunk, subset_indices)]
+
+        if len(index_chunk) == 0:
+            return [], []
+
+        # Distance calculation core code
+        if self.distance == 'cosine':
+            scores = cosine_distance(database_chunk, vector, device=self.device).squeeze()
+        else:
+            scores = euclidean_distance(database_chunk, vector, device=self.device).squeeze()
+
+        if scores.ndim == 0:
+            scores = [scores]
+
+        return index_chunk, scores
+
     @ParameterValuesAssert({'vector': lambda s: s.ndim == 1})
     @ParameterTypeAssert({
         'vector': np.ndarray, 'k': int, 'field': (None, str, list),
@@ -478,7 +519,7 @@ class MinVectorDB:
     })
     def query(self, vector, k: int = 12, field: str | list = None, normalize: bool = False, subset_indices=None):
         """
-        Query the database for the vectors most similar to the given vector.
+        Query the database for the vectors most similar to the given vector in batches.
 
         Parameters:
             vector (np.ndarray): The query vector.
@@ -499,39 +540,29 @@ class MinVectorDB:
             raise ValueError('database is empty.')
 
         vector = vector.astype(self.dtypes)
+
         vector = to_normalize(vector) if normalize else vector
         vector = vector.reshape(-1, 1)
 
         all_scores = []
         all_index = []
 
-        if isinstance(field, str):
-            field = [field]
+        # 设定合适的批次大小
+        batch_size = 10 if self.chunk_size > 100000 else 50
 
-        for database, index, vector_field in self._data_loader():
-            if field is not None:
-                database = database[np.isin(vector_field, field)]
-                index = index[np.isin(vector_field, field)]
+        with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+            for i in range(0, len(self._database_chunk_path), batch_size):
+                batch_paths = self._database_chunk_path[i:i + batch_size]
+                futures = [
+                    executor.submit(self._query_chunk, database, index, vector, field, subset_indices, vector_field)
+                    for database, index, vector_field in self._data_loader(batch_paths)]
 
-            if subset_indices is not None:
-                subset_indices = list(set(subset_indices))
-                database = database[np.isin(index, subset_indices)]
-                index = index[np.isin(index, subset_indices)]
+                for future in futures:
+                    index, scores = future.result()
+                    all_scores.extend(scores)
+                    all_index.extend(index)
 
-            if len(index) == 0:
-                continue
-
-            # Distance calculation core code
-            if self.distance == 'cosine':
-                batch_scores = cosine_distance(database, vector, self.device).squeeze()
-            else:
-                batch_scores = euclidean_distance(database, vector, self.device).squeeze()
-
-            if batch_scores.ndim == 0:
-                batch_scores = [batch_scores]
-
-            all_scores.extend(list(batch_scores))
-            all_index.extend(list(index))
+                del batch_paths, futures
 
         all_scores = np.asarray(all_scores)
         all_index = np.asarray(all_index)
