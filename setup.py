d8bf413d654f75963813fd7220a5cc702077625e
birchkwok <birchkwok@gmail.com>
2024-01-25 17:58:32 +0800

    release 0.1.2 version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='MinVectorDB',
-    version="0.1.1",
+    version="0.1.2",
     description='MinVectorDB is a simple vector storage and query database implementation, ' \
                 'providing clear and concise Python APIs aimed at lowering the barrier to using vector databases.',
     keywords='vector database',
