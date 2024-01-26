8e386adcdb8936d7177186bdab7075dd6e60bb8d
birchkwok <birchkwok@gmail.com>
2024-01-26 15:41:33 +0800

    Use msgpack to package indices and replace the original Trie tree with a hash table to enhance performance.

--- min_vec/ivf_index.py
+++ min_vec/ivf_index.py
@@ -1,155 +1,109 @@
-import struct
+import msgpack
 
 
-class TrieNode:
+class IndexNode:
     def __init__(self):
-        self.children = {}
-        self.is_end_of_word = False
-        self.indices = []
-        self.fields = []
-        self.paths = []
+        self.data = {}  # 使用一个字典来存储数据，键为(primary_key, attachment_msg)元组，值为路径
+
+    def add(self, index, field, path):
+        self.data[(index, field)] = path
+
+    def search(self, indices=None, fields=None):
+        # 如果没有任何搜索条件，直接返回所有记录
+        if indices is None and fields is None:
+            return [(k[0], k[1], v) for k, v in self.data.items()]
+
+        filtered_data = []
+
+        if indices is not None and fields is None:
+            indices_set = set(indices) if isinstance(indices, list) else {indices}
+            for (id, fd), path in self.data.items():
+                if id in indices_set:
+                    filtered_data.append((id, fd, path))
+            return filtered_data
+
+        if fields is not None and indices is None:
+            fields_set = set(fields) if isinstance(fields, list) else {fields}
+            for (id, fd), path in self.data.items():
+                if fd in fields_set:
+                    filtered_data.append((id, fd, path))
+            return filtered_data
+
+        if indices is not None and fields is not None:
+            indices_set = set(indices) if isinstance(indices, list) else {indices}
+            fields_set = set(fields) if isinstance(fields, list) else {fields}
+            for (id, fd), path in self.data.items():
+                if id in indices_set and fd in fields_set:
+                    filtered_data.append((id, fd, path))
+            return filtered_data
 
 
-class Trie:
+class HashIndex:
     def __init__(self):
-        self.root = TrieNode()
+        self.index = {}
 
-    def insert(self, cluster_id, index, field, path):
-        index = str(index)
-        field = str(field)
-        path = str(path)
-        node = self.root
-        for char in cluster_id:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
-        node.is_end_of_word = True
-        node.indices.append(index)
-        node.fields.append(field)
-        node.paths.append(path)
+    def insert(self, cluster_id, primary_key, attachment_msg, path):
+        if cluster_id not in self.index:
+            self.index[cluster_id] = IndexNode()
+        self.index[cluster_id].add(primary_key, attachment_msg, path)
 
     def search(self, cluster_id, indices=None, fields=None):
-        indices = [str(index) for index in indices] if indices is not None else None
-        fields = [str(field) for field in fields] if fields is not None else None
+        if cluster_id not in self.index:
+            return [], [], []
+        res = self.index[cluster_id].search(indices, fields)
+        if len(res) == 0:
+            return [], [], []
 
-        node = self._search_node(cluster_id)
-        if node is None or not node.is_end_of_word:
-            return None
-
-        filtered_indices, filtered_fields, filtered_paths = [], [], []
-
-        for idx, field, path in zip(node.indices, node.fields, node.paths):
-            index_condition = (indices is None) or (idx in indices)
-            field_condition = (fields is None) or (field in fields)
-
-            if index_condition and field_condition:
-                filtered_indices.append(idx)
-                filtered_fields.append(field)
-                filtered_paths.append(path)
-
-        return filtered_indices, filtered_fields, filtered_paths
-
-    def _search_node(self, cluster_id):
-        node = self.root
-        for char in cluster_id:
-            if char not in node.children:
-                return None
-            node = node.children[char]
-        return node
-
-    def save(self, file):
-        self._save_node(file, self.root)
-
-    def _save_node(self, f, node):
-        if node is None:
-            f.write(struct.pack('?', False))
-            return
-
-        f.write(struct.pack('?', True))
-        f.write(struct.pack('I', len(node.indices)))
-        for index in node.indices:
-            encoded_index = index.encode('utf-8')
-            f.write(struct.pack('I', len(encoded_index)))
-            f.write(encoded_index)
-
-        f.write(struct.pack('I', len(node.fields)))
-        for field in node.fields:
-            encoded_field = field.encode('utf-8')
-            f.write(struct.pack('I', len(encoded_field)))
-            f.write(encoded_field)
-
-        f.write(struct.pack('I', len(node.paths)))
-        for path in node.paths:
-            encoded_path = path.encode('utf-8')
-            f.write(struct.pack('I', len(encoded_path)))
-            f.write(encoded_path)
-
-        f.write(struct.pack('I', len(node.children)))
-        for char, child_node in node.children.items():
-            f.write(struct.pack('c', char.encode('utf-8')))
-            self._save_node(f, child_node)
-
-    def load(self, file):
-        self.root = self._load_node(file)
-
-    def _load_node(self, f):
-        exists = struct.unpack('?', f.read(1))[0]
-        if not exists:
-            return None
-
-        node = TrieNode()
-        node.is_end_of_word = True
-
-        indices_length = struct.unpack('I', f.read(4))[0]
-        node.indices = [f.read(struct.unpack('I', f.read(4))[0]).decode('utf-8') for _ in range(indices_length)]
-
-        fields_length = struct.unpack('I', f.read(4))[0]
-        node.fields = [f.read(struct.unpack('I', f.read(4))[0]).decode('utf-8') for _ in range(fields_length)]
-
-        paths_length = struct.unpack('I', f.read(4))[0]
-        node.paths = [f.read(struct.unpack('I', f.read(4))[0]).decode('utf-8') for _ in range(paths_length)]
-
-        children_length = struct.unpack('I', f.read(4))[0]
-        for _ in range(children_length):
-            char = struct.unpack('c', f.read(1))[0].decode('utf-8')
-            node.children[char] = self._load_node(f)
-
-        return node
+        idx, f, p = zip(*res)
+        return idx, f, p
 
 
 class CompactIVFIndex:
     def __init__(self, n_clusters):
-        self.ivf_index = {i: Trie() for i in range(n_clusters)}
+        self.index_ivf = {str(i): HashIndex() for i in range(n_clusters)}
 
     def add_to_cluster(self, cluster_id, index, field, path):
-        if cluster_id not in self.ivf_index:
+        cluster_id = str(cluster_id)
+        index = str(index)
+        field = str(field)
+        path = str(path)
+        if cluster_id not in self.index_ivf:
             raise ValueError(f"Cluster ID {cluster_id} does not exist")
-        self.ivf_index[cluster_id].insert(str(cluster_id), index, field, path)
+        self.index_ivf[cluster_id].insert(cluster_id, index, field, path)
 
     def search(self, cluster_id, indices=None, fields=None):
-        if cluster_id not in self.ivf_index:
-            return None
+        cluster_id = str(cluster_id)
+        if indices is not None:
+            indices = [str(i) for i in indices]
+        if fields is not None:
+            fields = [str(f) for f in fields]
 
-        trie = self.ivf_index[cluster_id]
-        return trie.search(str(cluster_id), indices, fields)
+        if cluster_id not in self.index_ivf:
+            return [], [], []
+
+        return self.index_ivf[cluster_id].search(cluster_id, indices=indices, fields=fields)
 
     def save(self, filename):
         with open(filename, 'wb') as f:
-            for cluster_id, trie in self.ivf_index.items():
-                f.write(struct.pack('i', cluster_id))
-                trie.save(f)
+            for cluster_id, index_hash in self.index_ivf.items():
+                for key, node in index_hash.index.items():
+                    packed_data = msgpack.packb((cluster_id, key, node.data))
+                    f.write(len(packed_data).to_bytes(4, byteorder='big'))
+                    f.write(packed_data)
 
     def load(self, filename):
         with open(filename, 'rb') as f:
-            self.ivf_index = {}
             while True:
-                bytes = f.read(4)
-                if len(bytes) < 4:
+                length_bytes = f.read(4)
+                if not length_bytes:
                     break
+                length = int.from_bytes(length_bytes, byteorder='big')
+                data = f.read(length)
+                cluster_id, key, node_data = msgpack.unpackb(data, use_list=False, strict_map_key=False, raw=False)
 
-                cluster_id = struct.unpack('i', bytes)[0]
-                trie = Trie()
-                trie.load(f)
-                self.ivf_index[cluster_id] = trie
+                if cluster_id not in self.index_ivf:
+                    self.index_ivf[cluster_id] = HashIndex()
+                self.index_ivf[cluster_id].index[key] = IndexNode()
+                self.index_ivf[cluster_id].index[key].data = node_data
 
         return self
