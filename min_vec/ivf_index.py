0d6f6241e4832a1dd6d9dff84ec7923ef2f27333
birchkwok <birchkwok@gmail.com>
2024-01-31 18:43:40 +0800

    Added a string pool class implementation and used the string pool to store the string content of IndexNode.

--- min_vec/ivf_index.py
+++ min_vec/ivf_index.py
@@ -1,40 +1,103 @@
 import msgpack
+from spinesUtils.asserts import raise_if_not
+
+
+class StringPool:
+    def __init__(self):
+        self.pool = {}  # 从字符串到数字标识符的映射
+        self.reverse_pool = {}  # 从数字标识符到字符串的映射
+        self.ids = {}  # 多个id共享一个字符串
+        self.next_id = 0  # 下一个可用的数字标识符
+
+    def intern(self, string, id=None):
+        raise_if_not(TypeError, isinstance(id, int) or id is None, "id must be an integer or None")
+
+        if id is not None:
+            next_id = id
+        else:
+            next_id = self.next_id
+
+        if string not in self.pool:
+            self.pool[string] = str(next_id)
+            self.reverse_pool[str(next_id)] = string
+
+        common_id = self.pool[string]
+        self.ids[str(next_id)] = common_id
+
+        self.next_id = next_id + 1
+
+
+        return self.pool[string]
+
+    def get_id(self, string):
+        try:
+            return self.pool[string]
+        except KeyError:
+            raise ValueError(f"'{string}' does not exist in the database.")
+
+    def get_string(self, id):
+        id = str(id)
+        return self.reverse_pool[self.ids[id]]
+
+
+    def save_to_disk(self, filename):
+        with open(filename, 'wb') as file:
+            data_to_save = {
+                'pool': self.pool,
+                'reverse_pool': self.reverse_pool,
+                'next_id': self.next_id,
+                'ids': self.ids
+            }
+            packed_data = msgpack.packb(data_to_save)
+            file.write(packed_data)
+
+    def load_from_disk(self, filename):
+        with open(filename, 'rb') as file:
+            data_loaded = msgpack.unpackb(file.read(), strict_map_key=False, raw=False, use_list=False)
+            self.pool = data_loaded['pool']
+            self.reverse_pool = data_loaded['reverse_pool']
+            self.next_id = data_loaded['next_id']
+            self.ids = data_loaded['ids']
+
+        return self
 
 
 class IndexNode:
     def __init__(self):
         self.data = {}  # 使用一个字典来存储数据，键为(primary_key, attachment_msg)元组，值为路径
 
-    def add(self, index, field, path):
-        self.data[(index, field)] = path
+    def add(self, index, field, path_id):
+        self.data[(index, field)] = path_id
 
-    def search(self, indices=None, fields=None):
+    def search(self, indices, fields, string_pool):
+        # 将fields转换为数字标识符
+        fields_set = None if fields is None else ({string_pool.get_id(f) for f in fields} if isinstance(fields, list)
+                                                 else {string_pool.get_id(fields)})
+
         # 如果没有任何搜索条件，直接返回所有记录
         if indices is None and fields is None:
-            return [(k[0], k[1], v) for k, v in self.data.items()]
+            return [(k[0], string_pool.get_string(k[1]), string_pool.get_string(v)) for k, v in self.data.items()]
 
         filtered_data = []
 
         if indices is not None and fields is None:
             indices_set = set(indices) if isinstance(indices, list) else {indices}
-            for (id, fd), path in self.data.items():
+            for (id, fd), path_id in self.data.items():
                 if id in indices_set:
-                    filtered_data.append((id, fd, path))
+                    filtered_data.append((id, string_pool.get_string(fd), string_pool.get_string(path_id)))
             return filtered_data
 
         if fields is not None and indices is None:
-            fields_set = set(fields) if isinstance(fields, list) else {fields}
-            for (id, fd), path in self.data.items():
+            for (id, fd), path_id in self.data.items():
                 if fd in fields_set:
-                    filtered_data.append((id, fd, path))
+                    filtered_data.append((id, string_pool.get_string(fd), string_pool.get_string(path_id)))
             return filtered_data
 
         if indices is not None and fields is not None:
             indices_set = set(indices) if isinstance(indices, list) else {indices}
-            fields_set = set(fields) if isinstance(fields, list) else {fields}
-            for (id, fd), path in self.data.items():
+            for (id, fd), path_id in self.data.items():
                 if id in indices_set and fd in fields_set:
-                    filtered_data.append((id, fd, path))
+                    filtered_data.append((id, string_pool.get_string(fd), string_pool.get_string(path_id)))
             return filtered_data
 
 
@@ -47,10 +110,10 @@ class HashIndex:
             self.index[cluster_id] = IndexNode()
         self.index[cluster_id].add(primary_key, attachment_msg, path)
 
-    def search(self, cluster_id, indices=None, fields=None):
+    def search(self, cluster_id, indices, fields, string_pool):
         if cluster_id not in self.index:
             return [], [], []
-        res = self.index[cluster_id].search(indices, fields)
+        res = self.index[cluster_id].search(indices, fields, string_pool)
         if len(res) == 0:
             return [], [], []
 
@@ -61,15 +124,16 @@ class HashIndex:
 class CompactIVFIndex:
     def __init__(self, n_clusters):
         self.index_ivf = {str(i): HashIndex() for i in range(n_clusters)}
+        self.string_pool = StringPool()  # 使用StringPool类
 
     def add_to_cluster(self, cluster_id, index, field, path):
         cluster_id = str(cluster_id)
         index = str(index)
-        field = str(field)
-        path = str(path)
+        field_id = self.string_pool.intern(str(field))
+        path_id = self.string_pool.intern(str(path))
         if cluster_id not in self.index_ivf:
             raise ValueError(f"Cluster ID {cluster_id} does not exist")
-        self.index_ivf[cluster_id].insert(cluster_id, index, field, path)
+        self.index_ivf[cluster_id].insert(cluster_id, index, field_id, path_id)
 
     def search(self, cluster_id, indices=None, fields=None):
         cluster_id = str(cluster_id)
@@ -81,29 +145,46 @@ class CompactIVFIndex:
         if cluster_id not in self.index_ivf:
             return [], [], []
 
-        return self.index_ivf[cluster_id].search(cluster_id, indices=indices, fields=fields)
+        return self.index_ivf[cluster_id].search(cluster_id, indices=indices, fields=fields,
+                                                 string_pool=self.string_pool)
 
     def save(self, filename):
-        with open(filename, 'wb') as f:
-            for cluster_id, index_hash in self.index_ivf.items():
-                for key, node in index_hash.index.items():
-                    packed_data = msgpack.packb((cluster_id, key, node.data))
-                    f.write(len(packed_data).to_bytes(4, byteorder='big'))
-                    f.write(packed_data)
+        filename = str(filename)
+        try:
+            with open(filename, 'wb') as f:
+                # 保存字符串池
+                self.string_pool.save_to_disk('.mvdb'.join(filename.split('.mvdb')[:-1]) + '_string_pool.mvdb')
+
+                # 保存其他数据
+                for cluster_id, hash_index in self.index_ivf.items():
+                    for key, node in hash_index.index.items():
+                        packed_data = msgpack.packb((cluster_id, key, node.data))
+                        f.write(len(packed_data).to_bytes(4, byteorder='big'))
+                        f.write(packed_data)
+        except IOError as e:
+            print(f"An error occurred while saving to {filename}: {e}")
 
     def load(self, filename):
-        with open(filename, 'rb') as f:
-            while True:
-                length_bytes = f.read(4)
-                if not length_bytes:
-                    break
-                length = int.from_bytes(length_bytes, byteorder='big')
-                data = f.read(length)
-                cluster_id, key, node_data = msgpack.unpackb(data, use_list=False, strict_map_key=False, raw=False)
+        filename = str(filename)
+        try:
+            with open(filename, 'rb') as f:
+                # 加载字符串池
+                self.string_pool.load_from_disk('.mvdb'.join(filename.split('.mvdb')[:-1]) + '_string_pool.mvdb')
 
-                if cluster_id not in self.index_ivf:
-                    self.index_ivf[cluster_id] = HashIndex()
-                self.index_ivf[cluster_id].index[key] = IndexNode()
-                self.index_ivf[cluster_id].index[key].data = node_data
+                # 加载其他数据
+                while True:
+                    length_bytes = f.read(4)
+                    if not length_bytes:
+                        break
+                    length = int.from_bytes(length_bytes, byteorder='big')
+                    data = f.read(length)
+                    cluster_id, key, node_data = msgpack.unpackb(data, use_list=False, raw=False, strict_map_key=False)
+
+                    if cluster_id not in self.index_ivf:
+                        self.index_ivf[cluster_id] = HashIndex()
+                    self.index_ivf[cluster_id].index[key] = IndexNode()
+                    self.index_ivf[cluster_id].index[key].data = node_data
+        except IOError as e:
+            print(f"An error occurred while loading from {filename}: {e}")
 
         return self
