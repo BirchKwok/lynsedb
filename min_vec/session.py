7c465e295553e1d10ac6f1a4c8cedf4ba495a2c7
birchkwok <birchkwok@gmail.com>
2024-02-26 18:11:00 +0800

    Security improvements

--- min_vec/session.py
+++ min_vec/session.py
@@ -9,7 +9,7 @@ class DatabaseSession:
         return self.db
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if not self.db._matrix_serializer._COMMIT_FLAG:
+        if not self.db._matrix_serializer.COMMIT_FLAG:
             self.db.commit()
 
         return False
