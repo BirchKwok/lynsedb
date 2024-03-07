a04d67b0591cd29996ba54ba5b51ba6e283eb9ca
birchkwok <birchkwok@gmail.com>
2024-03-07 10:51:01 +0800

    Update some variable names to make their meanings clearer.

--- min_vec/fields_mapper.py
+++ min_vec/fields_mapper.py
@@ -3,10 +3,10 @@
 
 class FieldsMapper:
     def __init__(self):
-        self.fields = {}
-        self.fields_mapper = {}
+        self.fields_int_mapper = {}  # id -> field
+        self.fields_str_mapper = {}  # field -> id
         self.fields_values = set()
-        self.last_id = 0
+        self.last_id = 0  # the initial id, the first id will be 0
 
     def _get_next_id(self):
         current_id = self.last_id
@@ -16,11 +16,11 @@ class FieldsMapper:
     def _single_encode(self, single_data):
         if single_data not in self.fields_values:
             self.fields_values.add(single_data)
-            self.fields[self.last_id] = single_data
-            self.fields_mapper[single_data] = self.last_id
+            self.fields_int_mapper[self.last_id] = single_data
+            self.fields_str_mapper[single_data] = self.last_id
             return self._get_next_id()
         else:
-            return self.fields_mapper[single_data]
+            return self.fields_str_mapper[single_data]
 
     def encode(self, data):
         if isinstance(data, str):
@@ -29,7 +29,7 @@ class FieldsMapper:
 
     def _single_decode(self, id):
         try:
-            return self.fields[id]
+            return self.fields_int_mapper[id]
         except KeyError:
             raise ValueError(f"Invalid id: {id}")
 
@@ -46,7 +46,7 @@ class FieldsMapper:
         import msgpack
         try:
             with open(filepath, 'wb') as f:
-                f.write(msgpack.packb([self.fields, self.fields_mapper, self.last_id]))
+                f.write(msgpack.packb([self.fields_int_mapper, self.fields_str_mapper, self.last_id]))
         except IOError as e:
             print(f"Error saving to file {filepath}: {e}")
 
@@ -54,9 +54,9 @@ class FieldsMapper:
         import msgpack
         try:
             with open(filepath, 'rb') as f:
-                self.fields, self.fields_mapper, self.last_id = msgpack.unpackb(f.read(), strict_map_key=False,
-                                                                                raw=False, use_list=False)
-            self.fields_values = set(self.fields_mapper.keys())
+                self.fields_int_mapper, self.fields_str_mapper, self.last_id = msgpack.unpackb(f.read(), strict_map_key=False,
+                                                                                               raw=False, use_list=False)
+            self.fields_values = set(self.fields_str_mapper.keys())
         except IOError as e:
             print(f"Error loading from file {filepath}: {e}")
         return self
