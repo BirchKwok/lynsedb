180b4c745d19f4fba7d8564ae1eeae973c55a02b
birchkwok <birchkwok@gmail.com>
2024-04-12 15:16:44 +0800

    update test_model.py

--- test/test_model.py
+++ test/test_model.py
@@ -28,13 +28,13 @@ def get_database_for_query(*args, with_field=True, field_prefix='test_', **kwarg
 
 def test_add_single_item_without_id_and_field():
     database = get_database()
-    id = database.add_item(np.ones(100), save_immediately=True)
+    id = database.add_item(np.ones(100))
+
+    database.commit()
 
     assert database._matrix_serializer.database == []
     assert database._matrix_serializer.fields == []
     assert database._matrix_serializer.indices == []
-
-    database.commit()
 
     assert database.shape == (1, 100)
 
@@ -43,13 +43,13 @@ def test_add_single_item_without_id_and_field():
 
 def test_add_single_item_with_id_and_field():
     database = get_database()
-    id = database.add_item(np.ones(100), index=1, field="test", save_immediately=True)
+    id = database.add_item(np.ones(100), index=1, field="test")
+
+    database.commit()
 
     assert database._matrix_serializer.database == []
     assert database._matrix_serializer.fields == []
     assert database._matrix_serializer.indices == []
-
-    database.commit()
 
     assert database.shape == (1, 100)
 
@@ -62,66 +62,31 @@ def test_bulk_add_item_without_id_and_field():
     for i in range(101):
         items.append((np.ones(100), ))
 
-    database.bulk_add_items(items, save_immediately=True)
+    database.bulk_add_items(items)
 
+    database.commit()
     assert database._matrix_serializer.database == []
     assert database._matrix_serializer.fields == []
     assert database._matrix_serializer.indices == []
-
-    database.commit()
 
     assert database.shape == (101, 100)
 
     database.delete()
 
 
-def test_add_single_item_with_vector_normalize():
-    database = get_database()
-    id = database.add_item(np.ones(100), index=1, field="test", save_immediately=True, normalize=True)
-
-    assert database._matrix_serializer.database == []
-    assert database._matrix_serializer.fields == []
-    assert database._matrix_serializer.indices == []
-
-    database.commit()
-
-    assert database.shape == (1, 100)
-
-    database.delete()
-
-
-def test_add_bulk_item_with_id_and_field():
+def test_bulk_add_item_with_id_and_field():
     database = get_database()
     items = []
     for i in range(101):
         items.append((np.ones(100), i, "test_" + str(i // 10)))
 
-    database.bulk_add_items(items, save_immediately=True)
+    database.bulk_add_items(items)
+
+    database.commit()
 
     assert database._matrix_serializer.database == []
     assert database._matrix_serializer.fields == []
     assert database._matrix_serializer.indices == []
-
-    database.commit()
-
-    assert database.shape == (101, 100)
-
-    database.delete()
-
-
-def test_add_bulk_item_with_normalize():
-    database = get_database()
-    items = []
-    for i in range(101):
-        items.append((np.ones(100), i, "test_" + str(i // 10)))
-
-    database.bulk_add_items(items, save_immediately=True, normalize=True)
-
-    assert database._matrix_serializer.database == []
-    assert database._matrix_serializer.fields == []
-    assert database._matrix_serializer.indices == []
-
-    database.commit()
 
     assert database.shape == (101, 100)
 
@@ -134,13 +99,13 @@ def test_add_bulk_item_with_id_and_chinese_field():
     for i in range(101):
         items.append((np.ones(100), i, "测试_" + str(i // 10)))
 
-    database.bulk_add_items(items, save_immediately=True)
+    database.bulk_add_items(items)
+
+    database.commit()
 
     assert database._matrix_serializer.database == []
     assert database._matrix_serializer.fields == []
     assert database._matrix_serializer.indices == []
-
-    database.commit()
 
     assert database.shape == (101, 100)
 
@@ -179,7 +144,7 @@ def test_query_with_normalize():
     database = get_database_for_query(with_field=True)
 
     vec = np.random.random(100)
-    n, d = database.query(vec, k=6, fields=['test_1'], normalize=True)
+    n, d = database.query(vec, k=6, fields=['test_1'])
 
     assert len(n) == len(d) == 6
     assert list(d) == sorted(d, key=lambda s: -s)
@@ -231,65 +196,78 @@ def test_query_with_chinese_list_field():
     database.delete()
 
 
-def test_query_stability_of_mvdb_files():
-    """Test whether the modification time of the mvdb file changes when querying multiple times."""
+def test_query_with_subset_indices():
     database = get_database_for_query(with_field=True)
+
     vec = np.random.random(100)
-    last_n, last_d = database.query(vec, k=6, fields=['test_1'])
-    assert len(last_n) == len(last_d) == 6
-    for i in range(20):
-        n, d = database.query(vec, k=6, fields=['test_1'])
-        assert last_d.tolist() == d.tolist()
-        assert last_n.tolist() == n.tolist()
-        assert len(n) == len(d) == 6
-        assert list(d) == sorted(d, key=lambda s: -s)
-        assert all(i in database._id_filter for i in n)
-        assert all(10 <= i < 20 for i in n)
+    n, d = database.query(vec, k=6, subset_indices=list(range(10)))
+
+    assert len(n) == len(d) == 6
+    assert list(d) == sorted(d, key=lambda s: -s)
+    assert all(i in database._id_filter for i in n)
+    assert all(i < 10 for i in n)
 
     database.delete()
 
 
-def test_lazy_bulk_add_items():
-    database = get_database()
-    items = []
-    for i in range(101):
-        items.append((np.ones(100), ))
+def test_query_with_subset_indices_and_field():
+    database = get_database_for_query(with_field=True, field_prefix='test_')
 
-    with database.insert_session():
-        database.bulk_add_items(items, save_immediately=False)
+    vec = np.random.random(100)
+    n, d = database.query(vec, k=6, fields=['test_0'], subset_indices=list(range(10)))
 
-    assert database.shape == (101, 100)
-    assert int(database.head(101).sum()) == 101 * 100
+    assert len(n) == len(d) == 6
+    assert list(d) == sorted(d, key=lambda s: -s)
+    assert all(i in database._id_filter for i in n)
+    assert all(i < 10 for i in n)
+
     database.delete()
 
 
-def test_lazy_add_item():
-    database = get_database()
-    id = database.add_item(np.ones(100), save_immediately=False)
+def test_query_with_subset_indices_and_list_field():
+    database = get_database_for_query(with_field=True)
 
-    database.commit()
-    assert database.shape == (1, 100)
-    assert int(database.head(1).sum()) == 100
+    vec = np.random.random(100)
+    n, d = database.query(vec, k=12, fields=['test_1', 'test_7'], subset_indices=list(range(10, 20)) + list(range(70, 80)))
+
+    assert len(n) == len(d) == 12
+    assert list(d) == sorted(d, key=lambda s: -s)
+    assert all(i in database._id_filter for i in n)
+    assert all((10 <= i < 20) or (70 <= i < 80) for i in n)
+
     database.delete()
 
 
-def test_lazy_add_item_and_lazy_bulk_add_items():
-    database = get_database()
-    items = []
-    for i in range(101):
-        items.append((np.ones(100), ))
+def test_query_with_subset_indices_and_chinese_field():
+    database = get_database_for_query(with_field=True, field_prefix='测试_')
 
-    with database.insert_session():
-        database.bulk_add_items(items, save_immediately=False)
-        database.add_item(np.ones(100), save_immediately=False)
+    vec = np.random.random(100)
+    n, d = database.query(vec, k=6, fields=['测试_0'], subset_indices=list(range(10)))
 
-    assert database.shape == (102, 100)
-    assert int(database.head(102).sum()) == 102 * 100
+    assert len(n) == len(d) == 6
+    assert list(d) == sorted(d, key=lambda s: -s)
+    assert all(i in database._id_filter for i in n)
+    assert all(i < 10 for i in n)
+
     database.delete()
 
 
-def test_query_stability_of_query_function():
-    """Test whether the modification time of the mvdb file changes when querying multiple times."""
+def test_query_with_subset_indices_and_chinese_list_field():
+    database = get_database_for_query(with_field=True, field_prefix='测试_')
+
+    vec = np.random.random(100)
+    n, d = database.query(vec, k=12, fields=['测试_1', '测试_7'],
+                          subset_indices=list(range(10, 20)) + list(range(70, 80)))
+
+    assert len(n) == len(d) == 12
+    assert list(d) == sorted(d, key=lambda s: -s)
+    assert all(i in database._id_filter for i in n)
+    assert all((10 <= i < 20) or (70 <= i < 80) for i in n)
+
+    database.delete()
+
+
+def test_query_stability_of_mvdb_files():
     database = get_database_for_query(with_field=True)
     vec = np.random.random(100)
     last_n, last_d = database.query(vec, k=6, fields=['test_1'])
@@ -313,21 +291,22 @@ def test_multiple_bulk_add_items():
     for i in range(101):
         items.append((np.ones(100), ))
 
-    database.bulk_add_items(items, save_immediately=False)
+    database.bulk_add_items(items)
     assert len(database._matrix_serializer.fields) == 101
     assert database._matrix_serializer.indices == list(range(101))
 
     database.commit()
     assert database.shape == (101, 100)
 
-    database.bulk_add_items(items, save_immediately=True)
+    database.bulk_add_items(items)
+    database.commit()
     assert database._matrix_serializer.fields == []
     assert database._matrix_serializer.indices == []
 
-    database.commit()
     assert database.shape == (202, 100)
 
     database.delete()
+
 
 def test_multiple_bulk_add_items_with_insert_session():
     database = get_database()
@@ -335,18 +314,19 @@ def test_multiple_bulk_add_items_with_insert_session():
     for i in range(101):
         items.append((np.ones(100), ))
 
-    database.bulk_add_items(items, save_immediately=False)
-    assert len(database._matrix_serializer.fields) == 101
-    assert database._matrix_serializer.indices == list(range(101))
+    with database.insert_session():
+        database.bulk_add_items(items)
+        assert len(database._matrix_serializer.fields) == 101
+        assert database._matrix_serializer.indices == list(range(101))
 
-    database.commit()
     assert database.shape == (101, 100)
 
-    database.bulk_add_items(items, save_immediately=True)
+    with database.insert_session():
+        database.bulk_add_items(items)
+
     assert database._matrix_serializer.fields == []
     assert database._matrix_serializer.indices == []
 
-    database.commit()
     assert database.shape == (202, 100)
 
     database.delete()
@@ -360,7 +340,7 @@ def test_multiple_initialization(dim=100, database_path='test_min_vec.mvdb', chu
         items.append((np.ones(100), i, "test_" + str(i // 10)))
 
     with database.insert_session():
-        database.bulk_add_items(items, save_immediately=False)
+        database.bulk_add_items(items)
     assert database._matrix_serializer.fields == []
     assert database._matrix_serializer.indices == []
 
@@ -374,7 +354,7 @@ def test_multiple_initialization(dim=100, database_path='test_min_vec.mvdb', chu
         items.append((np.ones(100), None, "test_" + str(i // 10)))
     # insert
     with database.insert_session():
-        database.bulk_add_items(items, save_immediately=True)
+        database.bulk_add_items(items)
 
     assert database._matrix_serializer.fields == []
     assert database._matrix_serializer.indices == []
@@ -383,31 +363,33 @@ def test_multiple_initialization(dim=100, database_path='test_min_vec.mvdb', chu
     database.delete()
 
 
-# Test if secondary initialization can properly initialize and insert
-def test_multiple_initialization_with_insert_session(dim=100, database_path='test_min_vec.mvdb', chunk_size=1000, dtypes='float32'):
-    database = MinVectorDB(dim=dim, database_path=database_path, chunk_size=chunk_size, dtypes=dtypes)
-    items = []
-    for i in range(101):
-        items.append((np.ones(100), i, "test_" + str(i // 10)))
+def test_result_order():
+    def get_test_vectors(shape):
+        for i in range(shape[0]):
+            yield np.random.random(shape[1])
 
-    with database.insert_session():
-        database.bulk_add_items(items, save_immediately=False)
-    assert database._matrix_serializer.fields == []
-    assert database._matrix_serializer.indices == []
+    for index_mode in ['FLAT', 'IVF-FLAT']:
+        # for distance in ['cosine', 'L2']:
+        db = MinVectorDB(dim=1024, database_path='test_min_vec.mvdb',
+                         chunk_size=10000, index_mode=index_mode)
 
-    assert database.shape == (101, 100)
-    del database
+        # You can perform this operation multiple times, and the data will be appended to the database.
+        with db.insert_session():
+            # Define the initial ID.
+            id = 0
+            vectors = []
+            for t in get_test_vectors((100000, 1024)):
+                if id == 0:
+                    query = t
+                vectors.append((t, id))
+                id += 1
 
-    database = MinVectorDB(dim=dim, database_path=database_path, chunk_size=chunk_size, dtypes=dtypes)
+            # Here, normalization can be directly specified, achieving the same effect as `t = t / np.linalg.norm(t) `.
+            db.bulk_add_items(vectors)
 
-    items = []
-    for i in range(101):
-        items.append((np.ones(100), None, "test_" + str(i // 10)))
-    # insert
-    with database.insert_session():
-        database.bulk_add_items(items, save_immediately=True)
-    assert database._matrix_serializer.fields == []
-    assert database._matrix_serializer.indices == []
+        index, score = db.query(query, k=10, return_similarity=True)
 
-    assert database.shape == (202, 100)
-    database.delete()
+        assert len(index) == len(score) == 10
+        assert index[0] == 0
+
+        db.delete()
