cf7ad79b3bd3671e5c4081c8dd631d4572e2ee12
birchkwok <birchkwok@gmail.com>
2024-04-12 15:14:10 +0800

    Remove distance parameter and add default reassignment_ratio=0 to prevent reassigning small clusters.

--- min_vec/data_structures/kmeans.py
+++ min_vec/data_structures/kmeans.py
@@ -2,12 +2,11 @@ from sklearn.cluster import MiniBatchKMeans
 
 
 class BatchKMeans:
-    def __init__(self, n_clusters, random_state=42, epochs=100, batch_size=1024, distance='euclidean'):
+    def __init__(self, n_clusters, random_state=42, epochs=100, batch_size=1024):
         self.model = MiniBatchKMeans(n_clusters=n_clusters, random_state=random_state, max_iter=epochs,
-                                     batch_size=batch_size, n_init='auto')
+                                     batch_size=batch_size, n_init='auto', reassignment_ratio=0)
 
         self.n_clusters = n_clusters
-        self.distance = distance
         self.epochs = epochs
         self.batch_size = batch_size
 
