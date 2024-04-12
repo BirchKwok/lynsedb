2ea7a2ce49a559d901919b0daa43030c39c0fd17
birchkwok <birchkwok@gmail.com>
2024-04-12 15:22:13 +0800

    update version to 0.2.5

--- min_vec/__init__.py
+++ min_vec/__init__.py
@@ -1,4 +1,5 @@
 
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 
-from min_vec.api.api import MinVectorDB, get_all_configs
+from min_vec.api.api import MinVectorDB
+from min_vec.configs.config import get_all_configs
