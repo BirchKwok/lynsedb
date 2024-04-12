516760c4cdfa63dec0f14486c29ef1cfe889ffa1
birchkwok <birchkwok@gmail.com>
2024-04-12 15:20:42 +0800

    Add LimitedSorted class to perform expensive decoding operations on a limited number of vectors.

--- min_vec/execution_layer/query.py
+++ min_vec/execution_layer/query.py
@@ -1,25 +1,27 @@
 """query.py: this file is used to query the database for the vectors most similar to the given vector."""
-import os
 from concurrent.futures import ThreadPoolExecutor
+import numpy as np
+from spinesUtils.asserts import raise_if
 
-from pyroaring import BitMap
+from min_vec.computational_layer.engines import to_normalize
+from min_vec.configs.config import config
+from min_vec.computational_layer.engines import argsort_topk, cosine_distance, euclidean_distance
+from min_vec.execution_layer.matrix_serializer import MatrixSerializer
+from min_vec.utils.utils import QueryVectorCache
+from min_vec.data_structures.limited_sort import LimitedSorted
 
-from min_vec.configs.config import *
-from min_vec.computational_layer.engines import argsort_topk
 
+class Query:
+    """Query the database for the vectors most similar to the given vector."""
 
-class DatabaseQuery:
-    from min_vec.utils.utils import QueryVectorCache
-
-    def __init__(self, matrix_serializer) -> None:
+    def __init__(self, matrix_serializer: MatrixSerializer, n_threads=10) -> None:
         """
         Query the database for the vectors most similar to the given vector.
 
         Parameters:
             matrix_serializer (MatrixSerializer): The database to be queried.
+            n_threads (int): The number of threads to use for querying the database.
         """
-        from min_vec.computational_layer.engines import cosine_distance, euclidean_distance
-
         self.matrix_serializer = matrix_serializer
 
         self.logger = self.matrix_serializer.logger
@@ -29,31 +31,31 @@ class DatabaseQuery:
         self.distance_func = cosine_distance if self.distance == 'cosine' else euclidean_distance
         self.is_reversed = -1 if self.distance == 'cosine' else 1
         self.chunk_size = self.matrix_serializer.chunk_size
-        self.device = self.matrix_serializer.device
 
         self.fields_mapper = self.matrix_serializer.fields_mapper
 
-        self.database = []
-        self.index = []
-        self.vector_field = []
+        self.n_threads = n_threads
 
-    def _query_chunk(self, database_chunk, index_chunk, vector_field, vector, field, subset_indices):
+        self.scaler = getattr(self.matrix_serializer, 'scaler', None)
+
+        self.executors = ThreadPoolExecutor(max_workers=self.n_threads)
+
+    def _query_chunk(self, database_chunk, index_chunk, vector_field, vector, field, subset_indices, limited_sorted):
         """
         Query a single database chunk for the vectors most similar to the given vector.
 
         Parameters:
             database_chunk (np.ndarray): The database chunk to be queried.
             index_chunk (np.ndarray): The indices of the vectors in the database chunk.
+            vector_field (np.ndarray): The field of the vectors.
             vector (np.ndarray): The query vector.
             field (str or list, optional): The target field for filtering the vectors.
             subset_indices (list, optional): The subset of indices to query.
-            vector_field (np.ndarray): The field of the vectors.
+            limited_sorted (HeapqSorted): The heapq object to store the top k nearest vectors.
 
         Returns:
             Tuple: The indices and similarity scores of the nearest vectors in the chunk.
         """
-        import numpy as np
-
         field_condition = None
         si_condition = None
 
@@ -85,19 +87,25 @@ class DatabaseQuery:
         if len(index_chunk) == 0:
             return [], []
 
+        if self.distance != 'cosine' and self.scaler is not None:
+            database_chunk = self.scaler.decode(database_chunk)
+
         # Distance calculation core code
-        scores = self.distance_func(vector, database_chunk, device=self.device)
+        scores = self.distance_func(vector, database_chunk)
 
         if scores.ndim == 0:
-            scores = [scores]
+            scores = np.array([scores])
         elif scores.ndim == 2:
             scores = scores.squeeze()
 
+        if limited_sorted is not None and self.distance == 'cosine':
+            limited_sorted.add(scores, index_chunk, database_chunk)
+
         return index_chunk, scores
 
-    @QueryVectorCache(MVDB_QUERY_CACHE_SIZE)
+    @QueryVectorCache(config.MVDB_QUERY_CACHE_SIZE)
     def query(self, vector, k: int | str = 12,
-              fields: list = None, normalize: bool = False, subset_indices=None, **kwargs):
+              fields: list = None, subset_indices=None, return_similarity=False, **kwargs):
         """
         Query the database for the vectors most similar to the given vector in batches.
 
@@ -105,8 +113,8 @@ class DatabaseQuery:
             vector (np.ndarray): The query vector.
             k (int or str): The number of nearest vectors to return. if be 'all', return all vectors.
             fields (list, optional): The target of the vector.
-            normalize (bool): Whether to normalize the input vector.
             subset_indices (list, optional): The subset of indices to query.
+            return_similarity (bool): Whether to return the similarity scores of the nearest vectors.
 
         Returns:
             Tuple: The indices and similarity scores of the top k nearest vectors.
@@ -114,16 +122,9 @@ class DatabaseQuery:
         Raises:
             ValueError: If the database is empty.
         """
-
-        import numpy as np
-        from spinesUtils.asserts import raise_if
-
-        from min_vec.computational_layer.engines import to_normalize
-
         self.logger.debug(f'Query vector: {vector.tolist()}')
         self.logger.debug(f'Query k: {k}')
         self.logger.debug(f'Query fields: {fields}')
-        self.logger.debug(f'Query normalize: {normalize}')
         self.logger.debug(f'Query subset_indices: {subset_indices}')
 
         raise_if(TypeError, not isinstance(k, int) and not (isinstance(k, str) and k != 'all'),
@@ -133,14 +134,12 @@ class DatabaseQuery:
                  'fields must be list or None.')
         raise_if(ValueError, not isinstance(subset_indices, list) and subset_indices is not None,
                  'subset_indices must be list or None.')
-        raise_if(TypeError, not isinstance(normalize, bool), 'normalize must be bool.')
         raise_if(ValueError, vector is None, 'vector must be not None.')
         raise_if(ValueError, len(vector) != self.matrix_serializer.shape[1],
                  'vector must be same dim with database.')
         raise_if(ValueError, not isinstance(vector, np.ndarray), 'vector must be np.ndarray.')
         raise_if(ValueError, vector.ndim != 1, 'vector must be 1d array.')
 
-        # fields = np.array(fields) if fields is not None else fields
         if self.matrix_serializer.shape[0] == 0:
             raise ValueError('database is empty.')
 
@@ -149,13 +148,15 @@ class DatabaseQuery:
 
         vector = vector.astype(self.dtypes) if vector.dtype != self.dtypes else vector
 
-        vector = to_normalize(vector) if normalize else vector
+        vector = to_normalize(vector)
 
-        vector = vector.reshape(1, -1) if vector.ndim == 1 else vector
+        if self.scaler is not None and return_similarity and self.distance == 'cosine':
+            limited_sorted = LimitedSorted(vector, k, self.scaler)
+        else:
+            limited_sorted = None
 
         all_scores = []
         all_index = []
-        unique_indices = BitMap()
 
         def sort_results(all_s, all_i):
             all_scores_i = np.array(all_s)
@@ -166,36 +167,32 @@ class DatabaseQuery:
             return all_index_i[top_k_indices], all_scores_i[top_k_indices]
 
         def batch_query(vector, fields=None, subset_indices=None, is_ivf=True, cluster_id=None, sort=True):
-            nonlocal all_scores, all_index
+            nonlocal all_scores, all_index, limited_sorted
 
             dataloader = self.matrix_serializer.cluster_dataloader(cluster_id, mode='lazy') \
                 if is_ivf and self.matrix_serializer.ann_model \
                 else self.matrix_serializer.iterable_dataloader(mode='lazy')
 
-            with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
-                futures = [i for i in executor.map(
-                    lambda x: self._query_chunk(x[0], x[1], x[2], vector, fields, subset_indices), dataloader)
-                    if len(i[0]) != 0]
+            futures = [i for i in self.executors.map(
+                lambda x: self._query_chunk(x[0], x[1], x[2], vector, fields,
+                                            subset_indices, limited_sorted), dataloader)
+                       if len(i[0]) != 0]
 
             if not futures:
                 return [], []
+
+            if return_similarity and self.scaler is not None and sort and self.distance == 'cosine':
+                return limited_sorted.get_top_n()
 
             index, scores = zip(*futures)
 
             # if index[0] is iterable, then index is a tuple of numpy ndarray, so we need to flatten it
             if len(index[0].shape) > 0:
-                index = [item for sublist in index for item in sublist]
-                scores = [item for sublist in scores for item in sublist]
+                index = np.concatenate(index).ravel()
+                scores = np.concatenate(scores).ravel()
 
-            index = np.array(index)
-            scores = np.array(scores)
-
-            if len(index) != 0:
-                new_index = index[~np.isin(index, unique_indices.to_array())]
-                new_scores = scores[~np.isin(index, unique_indices.to_array())]
-                all_scores.extend(new_scores)
-                all_index.extend(new_index)
-                unique_indices.update(new_index)
+            all_scores.extend(scores)
+            all_index.extend(index)
 
             return sort_results(all_scores, all_index) if sort else None
 
@@ -204,8 +201,12 @@ class DatabaseQuery:
             return batch_query(vector, fields, subset_indices, False)
 
         # otherwise, use IVF-FLAT
-        cluster_distances = self.distance_func(vector, self.matrix_serializer.ann_model.cluster_centers_,
-                                               device=self.device).squeeze()
+        if self.distance != 'cosine' and self.scaler is not None:
+            cluster_distances = self.distance_func(vector,
+                                                   self.scaler.decode(
+                                                       self.matrix_serializer.ann_model.cluster_centers_)).squeeze()
+        else:
+            cluster_distances = self.distance_func(vector, self.matrix_serializer.ann_model.cluster_centers_).squeeze()
 
         cluster_id_sorted = np.argsort(cluster_distances)[::-1] if self.distance == 'cosine' else np.argsort(
             cluster_distances)
@@ -216,4 +217,14 @@ class DatabaseQuery:
             if len(all_index) >= k:
                 break
 
+        if return_similarity and self.scaler is not None and self.distance == 'cosine':
+            return limited_sorted.get_top_n()
+
         return sort_results(all_scores, all_index)
+
+    def __del__(self):
+        self.executors.shutdown(wait=True)
+        self.logger.debug('DatabaseQuery executor shutdown.')
+
+    def delete(self):
+        self.__del__()
