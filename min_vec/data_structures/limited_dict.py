6f6c71a6d09dc60429babd63e31f1ac301459e29
birchkwok <birchkwok@gmail.com>
2024-04-12 15:15:07 +0800

    Implement using OrderedDict to reduce the need for maintaining separate order lists.

--- min_vec/data_structures/limited_dict.py
+++ min_vec/data_structures/limited_dict.py
@@ -1,26 +1,48 @@
+from collections import OrderedDict
 
 
-class LimitedDict(dict):
+class LimitedDict:
     def __init__(self, max_size):
         self.max_size = max_size
-        self.keys = []
-        super().__init__()
+        self.cache = OrderedDict()
 
     def __setitem__(self, key, value):
-        if key not in self.keys:
-            if len(self.keys) >= self.max_size:
-                del self[self.keys[0]]
-                self.keys.pop(0)
-            self.keys.append(key)
-        super().__setitem__(key, value)
+        # 先检查键是否已存在，如果是，则先删除旧键
+        if key in self.cache:
+            del self.cache[key]
+        self.cache[key] = value
+        # 如果缓存超出最大大小，移除最老的项
+        if len(self.cache) > self.max_size:
+            self.cache.popitem(last=False)
 
     def __getitem__(self, key):
-        return super().get(key, None)
+        try:
+            # 移动到末尾，表示最近使用
+            value = self.cache.pop(key)
+            self.cache[key] = value
+            return value
+        except KeyError:
+            raise KeyError('Key not found')
 
-    def __delitem__(self, key):
-        if key in self.keys:
-            self.keys.remove(key)
-        super().__delitem__(key)
+    def get(self, key, default=None):
+        if key not in self.cache:
+            return default
+        return self.__getitem__(key)
+
+    def clear(self):
+        self.cache.clear()
+
+    def keys(self):
+        return self.cache.keys()
+
+    def pop(self, key, default=None):
+        return self.cache.pop(key, default)
+
+    def __contains__(self, key):
+        return key in self.cache
+
+    def __len__(self):
+        return len(self.cache)
 
     def __repr__(self):
-        return str({k: self[k] for k in self.keys})
+        return repr(self.cache)
