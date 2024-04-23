1f7b2eed712919a0bfbd8eff88b57643ee76487d
birchkwok <birchkwok@gmail.com>
2024-04-23 21:34:50 +0800

    move file to new module

---
+++ min_vec/structures/limited_dict.py
@@ -0,0 +1,56 @@
+from collections import OrderedDict
+import threading
+
+
+class LimitedDict:
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.cache = OrderedDict()
+        self.lock = threading.RLock()
+
+    def __setitem__(self, key, value):
+        with self.lock:
+            if key in self.cache:
+                del self.cache[key]
+            self.cache[key] = value
+            if len(self.cache) > self.max_size:
+                self.cache.popitem(last=False)
+
+    def __getitem__(self, key):
+        with self.lock:
+            try:
+                value = self.cache.pop(key)
+                self.cache[key] = value
+                return value
+            except KeyError:
+                raise KeyError('Key not found')
+
+    def get(self, key, default=None):
+        with self.lock:
+            if key not in self.cache:
+                return default
+            return self.__getitem__(key)
+
+    def clear(self):
+        with self.lock:
+            self.cache.clear()
+
+    def keys(self):
+        with self.lock:
+            return self.cache.keys()
+
+    def pop(self, key, default=None):
+        with self.lock:
+            return self.cache.pop(key, default)
+
+    def __contains__(self, key):
+        with self.lock:
+            return key in self.cache
+
+    def __len__(self):
+        with self.lock:
+            return len(self.cache)
+
+    def __repr__(self):
+        with self.lock:
+            return repr(self.cache)
