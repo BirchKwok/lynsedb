9cc2b21039721e10cfc17c8300981ca60842a3e6
birchkwok <birchkwok@gmail.com>
2024-04-26 18:03:19 +0800

    update requirements.txt

--- min_vec/__init__.py
+++ min_vec/__init__.py
@@ -19,17 +19,16 @@ class MinVectorDB:
     from typing import Union
 
     def __new__(cls, root_path: Union[str, Path]):
+        from pathlib import Path
         from min_vec.api.high_level import MinVectorDBLocalClient
         from min_vec.api.client_api import MinVectorDBHTTPClient
 
-        if isinstance(root_path, MinVectorDB.Path):
+        if isinstance(root_path, Path):
             root_path = root_path.as_posix()
 
         if root_path.startswith('http://') or root_path.startswith('https://'):
-            instance = super(MinVectorDB, cls).__new__(MinVectorDBHTTPClient)
+            instance = MinVectorDBHTTPClient(url=root_path)
         else:
-            instance = super(MinVectorDB, cls).__new__(MinVectorDBLocalClient)
-
-        instance.__init__(root_path)
+            instance = MinVectorDBLocalClient(root_path)
 
         return instance
