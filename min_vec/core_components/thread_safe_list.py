99f44b55d92c714a5fe305855ade112c1c4b78df
birchkwok <birchkwok@gmail.com>
2024-05-04 21:00:46 +0800

    Change the use of threading's RLock to the internal ThreadLock of min_vec.

--- min_vec/core_components/thread_safe_list.py
+++ min_vec/core_components/thread_safe_list.py
@@ -1,42 +1,42 @@
-import threading
+from min_vec.core_components.cross_lock import ThreadLock
 
 
-class ThreadSafeList(list):
+class SafeList(list):
     def __init__(self, *args, **kwargs):
-        super(ThreadSafeList, self).__init__(*args, **kwargs)
-        self.lock = threading.RLock()
+        super(SafeList, self).__init__(*args, **kwargs)
+        self.lock = ThreadLock()
 
     def append(self, item):
         with self.lock:
-            super(ThreadSafeList, self).append(item)
+            super(SafeList, self).append(item)
 
     def pop(self, index=-1):
         with self.lock:
-            return super(ThreadSafeList, self).pop(index)
+            return super(SafeList, self).pop(index)
 
     def __getitem__(self, index):
         with self.lock:
-            return super(ThreadSafeList, self).__getitem__(index)
+            return super(SafeList, self).__getitem__(index)
 
     def __setitem__(self, index, value):
         with self.lock:
-            return super(ThreadSafeList, self).__setitem__(index, value)
+            return super(SafeList, self).__setitem__(index, value)
 
     def __delitem__(self, index):
         with self.lock:
-            return super(ThreadSafeList, self).__delitem__(index)
+            return super(SafeList, self).__delitem__(index)
 
     def extend(self, iterable):
         with self.lock:
-            return super(ThreadSafeList, self).extend(iterable)
+            return super(SafeList, self).extend(iterable)
 
     def remove(self, item):
         with self.lock:
-            return super(ThreadSafeList, self).remove(item)
+            return super(SafeList, self).remove(item)
 
     def __len__(self):
         with self.lock:
-            return super(ThreadSafeList, self).__len__()
+            return super(SafeList, self).__len__()
 
     def __iter__(self):
         with self.lock:
@@ -44,8 +44,8 @@ class ThreadSafeList(list):
 
     def __str__(self):
         with self.lock:
-            return super(ThreadSafeList, self).__str__()
+            return super(SafeList, self).__str__()
 
     def clear(self):
         with self.lock:
-            return super(ThreadSafeList, self).clear()
+            return super(SafeList, self).clear()
