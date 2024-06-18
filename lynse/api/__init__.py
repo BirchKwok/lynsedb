0a311d50bcc86895bc16f90a9b947cf57295a164
birchkwok <birch.kwok@tvbc.com.cn>
2024-06-18 17:26:32 +0800

    rename package

--- lynse/api/__init__.py
+++ lynse/api/__init__.py
@@ -5,7 +5,7 @@ from lynse.configs.config import config
 
 logger = Logger(
     fp=config.LYNSE_LOG_PATH,
-    name='Convergence',
+    name='LynseDB',
     truncate_file=config.LYNSE_TRUNCATE_LOG,
     with_time=config.LYNSE_LOG_WITH_TIME,
     level=config.LYNSE_LOG_LEVEL
