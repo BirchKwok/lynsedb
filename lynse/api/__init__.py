81af3a4229413976d0b8eb8ff01521cd3729bffa
birchkwok <birch.kwok@tvbc.com.cn>
2024-08-15 19:09:35 +0800

    Support user configuration of environment variables to change default settings

--- lynse/api/__init__.py
+++ lynse/api/__init__.py
@@ -1,12 +1,13 @@
+import os
 from spinesUtils.logging import Logger
 
-from lynse.configs.config import config
+from ..configs.config import config
 
-
+# Create a logger for the LynseDB
 logger = Logger(
-    fp=config.LYNSE_LOG_PATH,
+    fp=os.environ.get('LYNSE_LOG_PATH') or config.LYNSE_LOG_PATH,
     name='LynseDB',
-    truncate_file=config.LYNSE_TRUNCATE_LOG,
-    with_time=config.LYNSE_LOG_WITH_TIME,
-    level=config.LYNSE_LOG_LEVEL
+    truncate_file=os.environ.get('LYNSE_TRUNCATE_LOG_FILE') or config.LYNSE_TRUNCATE_LOG,
+    with_time=os.environ.get('LYNSE_LOG_WITH_TIME') or config.LYNSE_LOG_WITH_TIME,
+    level=os.environ.get('LYNSE_LOG_LEVEL') or config.LYNSE_LOG_LEVEL
 )
