aff4520f1765bc41c903a17a2b3fec030fe0ff50
birchkwok <birch.kwok@tvbc.com.cn>
2024-08-15 18:53:35 +0800

    Add fingerprint identifier

--- lynse/api/native_api/high_level.py
+++ lynse/api/native_api/high_level.py
@@ -1,13 +1,15 @@
 import json
 import shutil
+import uuid
 from functools import partial
 from pathlib import Path
 from typing import Union
 
 import pandas as pd
+from spinesUtils.asserts import ParameterTypeAssert
 
-from lynse.api import logger
-from lynse.utils.utils import unavailable_if_deleted
+from ...api import logger
+from ...utils.utils import unavailable_if_deleted
 
 
 class _Register:
@@ -102,13 +104,17 @@ class _Register:
         details = pd.DataFrame(details).T
         return details
 
-    def update_description(self, collection: str, description: str):
+    @ParameterTypeAssert({
+        'collection': str,
+        'description': (None, str, int, float, bool)
+    })
+    def update_description(self, collection: str, description: Union[None, str, int, float, bool]):
         """
         Update the description of the collection.
 
         Parameters:
             collection (str): The name of the collection.
-            description (str): The description of the collection.
+            description (None or str or int or float or bool): The description of the collection.
         """
         collections = self.get_collections_details()
 
@@ -146,6 +152,11 @@ class LocalClient:
 
         Path(cls._last_root_path).mkdir(parents=True, exist_ok=True)
 
+        if not (Path(cls._last_root_path) / '.fingerprint').exists():
+            # create a database fingerprint
+            with open(Path(cls._last_root_path) / '.fingerprint', 'w') as f:
+                f.write(uuid.uuid4().hex)
+
         return cls._instance
 
     def _init(self, root_path: Union[Path, str]):
@@ -160,11 +171,12 @@ class LocalClient:
 
     @unavailable_if_deleted
     def require_collection(
-            self, collection: str, dim: int = None,
+            self,
+            collection: str,
+            dim: int = None,
             chunk_size: int = 100_000,
-            distance: str = 'cosine',
             dtypes: str = 'float32',
-            use_cache: bool = True, scaler_bits: Union[int, None] = 8,
+            use_cache: bool = True,
             n_threads: Union[int, None] = 10,
             warm_up: bool = False,
             drop_if_exists: bool = False,
@@ -173,22 +185,15 @@ class LocalClient:
     ):
         """Create or load a collection in the database.
 
-        Initialize the vector database.
-
         Parameters:
+            collection (str): The name of the collection.
             dim (int): Dimension of the vectors. Default is None.
                 When creating a new collection, the dimension of the vectors must be specified.
                 When loading an existing collection, the dimension of the vectors is automatically loaded.
-            collection (str): The name of the collection.
             chunk_size (int): The size of each data chunk. Default is 100_000.
-            distance (str): Method for calculating vector distance.
-                Options are 'cosine' or 'L2' for Euclidean distance. Default is 'cosine'.
             dtypes (str): The data type of the vectors. Default is 'float32'.
                 Options are 'float16', 'float32' or 'float64'.
             use_cache (bool): Whether to use cache for search. Default is True.
-            scaler_bits (int): The number of bits for scalar quantization.
-                Options are 8, 16, or 32. The default is None, which means no scalar quantization.
-                The 8 for 8-bit, 16 for 16-bit, and 32 for 32-bit.
             n_threads (int): The number of threads to use for parallel processing. Default is 10.
             warm_up (bool): Whether to warm up the database. Default is False.
             drop_if_exists (bool): Whether to drop the collection if it already exists. Default is False.
@@ -199,7 +204,7 @@ class LocalClient:
         Raises:
             ValueError: If `chunk_size` is less than or equal to 1.
         """
-        from lynse.api.native_api.low_level import ExclusiveDB
+        from ...api.native_api.low_level import ExclusiveDB
 
         collection_path = self._root_path / collection
 
@@ -207,16 +212,6 @@ class LocalClient:
             raise ValueError('Description must be a string')
         elif description is not None and len(description) > 500:
             raise ValueError('Description must be less than 500 characters')
-
-        logger.info(f"Creating collection {collection} with: \n "
-                    f"\r//    dim={dim}, collection='{collection}', \n"
-                    f"\r//    chunk_size={chunk_size}, distance='{distance}', \n"
-                    f"\r//    dtypes='{dtypes}', use_cache={use_cache}, \n"
-                    f"\r//    scaler_bits={scaler_bits}, n_threads={n_threads}, \n"
-                    f"\r//    warm_up={warm_up}, drop_if_exists={drop_if_exists}, \n"
-                    f"\r//    description="
-                    f"{description if description is None or len(description) <= 50 else description[:50] + '...'}, \n"
-                    )
 
         if collection in self._register:
             if drop_if_exists:
@@ -232,13 +227,13 @@ class LocalClient:
 
         self._collections[collection] = ExclusiveDB(
             dim=dim, database_path=collection_path.as_posix(), chunk_size=chunk_size, dtypes=dtypes,
-            distance=distance, use_cache=use_cache, scaler_bits=scaler_bits, n_threads=n_threads,
+            use_cache=use_cache, n_threads=n_threads,
             warm_up=warm_up, initialize_as_collection=True, cache_chunks=cache_chunks
         )
 
         self._register.register_collection(
             collection, dim=dim, database_path=collection_path.as_posix(), chunk_size=chunk_size, dtypes=dtypes,
-            distance=distance, use_cache=use_cache, scaler_bits=scaler_bits, n_threads=n_threads,
+            use_cache=use_cache, n_threads=n_threads,
             warm_up=warm_up, initialize_as_collection=True, description=description, cache_chunks=cache_chunks
         )
 
@@ -259,7 +254,7 @@ class LocalClient:
         Returns:
             ExclusiveDB: The collection.
         """
-        from lynse.api.native_api.low_level import ExclusiveDB
+        from ...api.native_api.low_level import ExclusiveDB
 
         if collection not in self._collections:
             if collection not in self._register:
@@ -272,6 +267,23 @@ class LocalClient:
             self._collections[collection] = ExclusiveDB(**params)
 
         return self._collections[collection]
+
+    @unavailable_if_deleted
+    def copy_collection(self, deep=False):
+        """
+        Copy the collection.
+
+        Parameters:
+            deep (bool): Whether to make a deep copy. Default is False.
+                Which means share the data with the original collection.
+
+        Returns:
+            ExclusiveDB: The copied collection.
+
+        Raises:
+            NotImplementedError: This method is not implemented yet.
+        """
+        raise NotImplementedError("This method is not implemented yet.")
 
     @unavailable_if_deleted
     def show_collections(self):
@@ -325,13 +337,13 @@ class LocalClient:
         LocalClient._instance = None
         self.STATUS = 'DELETED'
 
-    def update_collection_description(self, collection: str, description: str):
+    def update_collection_description(self, collection: str, description: Union[None, str, int, float, bool]):
         """
         Update the description of the collection.
 
         Parameters:
             collection (str): The name of the collection.
-            description (str): The description of the collection.
+            description (None or str or int or float or bool): The description of the collection.
         """
         self._register.update_description(collection, description)
 
