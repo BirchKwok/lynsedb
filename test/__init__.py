c17d5567f5f9b8172648700fb44228f9ada74fc7
birchkwok <birch.kwok@tvbc.com.cn>
2024-08-17 15:46:28 +0800

    Rename module

--- test/__init__.py
+++ test/__init__.py
@@ -1,5 +1,5 @@
 from lynse.api.native_api.high_level import LocalClient
 from lynse.api.native_api.low_level import ExclusiveDB
-from lynse.core_components.kv_cache import Filter, FieldCondition, MatchField, MatchID
+from lynse.core_components.fields_cache import Filter, FieldCondition, MatchField, MatchID
 from lynse import VectorDBClient
 from lynse.api.http_api.client_api import HTTPClient, Collection
