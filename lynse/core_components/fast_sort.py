26d76d97b84d5c56b3e91fa014845e7e5cab7267
birchkwok <birch.kwok@tvbc.com.cn>
2024-09-05 12:41:32 +0800

    Removed redundant logic.

--- lynse/core_components/fast_sort.py
+++ lynse/core_components/fast_sort.py
@@ -2,31 +2,27 @@ import numpy as np
 
 
 class FastSort:
-    def __init__(self, data, backend='numpy'):
+    def __init__(self, data):
         self.data = np.asarray(data) if not isinstance(data, np.ndarray) else data
-        self.backend = backend
-        if backend == 'jax':
-            import jax.numpy as jnp
-            self.np = jnp
-        else:
-            self.np = np
+        self.data = self.data.squeeze()
 
     def topk(self, k, ascending=True):
-        if len(self.data) < 10000:
-            sorted_indices = self.np.argsort(self.data)
-            if ascending:
-                topk_indices = sorted_indices[:k]
-            else:
-                topk_indices = sorted_indices[-k:][::-1]
+        axis = 0
+        k = min(k, self.data.shape[axis])
+        
+        if ascending:
+            partitioned_indices = np.argpartition(self.data, k-1, axis=axis)
+            topk_indices = partitioned_indices[:k]
         else:
-            if ascending:
-                partitioned_indices = self.np.argpartition(self.data, k-1)
-                topk_indices = partitioned_indices[:k]
-                topk_indices = topk_indices[self.np.argsort(self.data[topk_indices])]
-            else:
-                partitioned_indices = self.np.argpartition(self.data, len(self.data) - k)
-                topk_indices = partitioned_indices[-k:]
-                topk_indices = topk_indices[self.np.argsort(self.data[topk_indices])[::-1]]
+            partitioned_indices = np.argpartition(self.data, self.data.shape[1] - k, axis=axis)
+            topk_indices = partitioned_indices[-k:]
 
-        topk_values = self.data[topk_indices]
-        return np.array(topk_indices), np.array(topk_values)
+        topk_values = np.take_along_axis(self.data, topk_indices, axis=axis)
+        sorted_indices = np.argsort(topk_values, axis=axis)
+        if not ascending:
+            sorted_indices = sorted_indices[::-1]
+
+        topk_indices = np.take_along_axis(topk_indices, sorted_indices, axis=axis)
+
+        topk_values = np.take_along_axis(self.data, topk_indices, axis=axis)
+        return topk_indices, topk_values
